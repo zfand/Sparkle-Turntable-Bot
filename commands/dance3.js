39b23d1bf8adb050e37f2076898d73a8cfd4274f
zfand <zfand@zynga.com>
2013-05-02 22:45:47 -0700

    Changed bonus command to fist bump command

--- commands/dance3.js
+++ commands/dance3.js
@@ -1,15 +1,14 @@
-exports.name = 'bonus';
+exports.name = 'fist bump';
 exports.hidden = true;
 exports.enabled = true;
 exports.matchStart = false;
 exports.handler = function(data) {
     if ((config.bonusvote == 'CHAT') && (bonuspoints.indexOf(data.name) == -1)) {
         bonuspoints.push(data.name);
-        var target = getTarget();
         //If the target has been met, the bot will awesome
-        if((bonuspoints.length >= target) && !bonusvote 
+        if(!bonusvote 
             && (currentsong.djid != config.botinfo.userid)) {
-            bot.speak('Bonus!');
+            bot.speak(':fist:');
             bot.vote('up');
             bonusvote = true;
         }
