a2b4ede052a57063d67efab86d6daf70b079d212
zfand <zfand@zynga.com>
2013-05-25 23:31:19 -0700

    Fixed no yelling

--- commands/NOYELLING.js
+++ commands/NOYELLING.js
@@ -1,12 +1,35 @@
+var output = ["SHHHHHHH", "SHHHHHHH", "SHH!", "NO YELLING", "CAPS LOCK BRAH", "SHHH DUNKEY", "LET ME TELL YOU A LITTLE STORY ABOUT A MAN NAMED SHH","SHH", "http://i.imgur.com/V0y1tXL.gif", "http://i.imgur.com/YGi0gkl.gif", "http://i.imgur.com/5lg65WI.gif"];
 exports.name = '';
 exports.hidden = true;
-exports.enabled = false;
+exports.enabled = true;
 exports.matchStart = false;
 exports.isValidFor = function(data) {
-    return ( data.text == data.text.toUpperCase() ); 
+	var text = data.text.replace(/[^a-zA-Z ]/g, "");
+	if (text.length > 1) {
+
+		//skip I
+		if (text == "I") {
+			return false;
+		}
+		//cheat to not detect emoticons
+		if (data.text[0] == ":" || data.text[0] == ";") {
+			return false;
+		}
+		//skip BAM!
+		if (text.toLowerCase().indexOf("bam") >= 0) {
+			return false;
+		}
+
+
+		//now heck
+		if (text == text.toUpperCase()) {
+			return true;
+		}
+	}
+    return false;
 }
 exports.handler = function(data) {
     if (data.userid != config.botinfo.userid) {
-        bot.speak('DUNKEY SHHHH');
+        bot.speak(output[Math.floor(Math.random()*output.length)]);
     }
 }
