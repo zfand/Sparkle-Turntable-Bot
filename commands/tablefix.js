c11d05cbb804a9946e6a63418227924fb9e4f416
zfand <zfand@zynga.com>
2013-05-25 23:32:59 -0700

    added table fix

---
+++ commands/tablefix.js
@@ -0,0 +1,9 @@
+exports.name = '/tableflip';
+exports.hidden = true;
+exports.enabled = true;
+exports.matchStart = false;
+exports.handler = function(data) {
+    if (data.userid != config.botinfo.userid) {
+        bot.speak('/tablefix');
+    }
+}
